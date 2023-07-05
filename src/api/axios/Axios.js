fd954ede2064106e0c33edae4642006b6444d735
Arjun <arjun.vkv.97@gmail.com>
2023-07-05 23:14:33 +0530

    removed console

--- src/api/axios/Axios.js
+++ src/api/axios/Axios.js
@@ -4,11 +4,11 @@ const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    console.log("error", error);
-  }
-);
+// axios.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     console.log("error", error);
+//   }
+// );
 
 export default instance;
