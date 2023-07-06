177655582426b8d3d7c3d84b1bfead43b0419fa0
Arjun <arjun.vkv.97@gmail.com>
2023-07-06 15:02:45 +0530

    image responsive

--- src/components/MovieCards/index.jsx
+++ src/components/MovieCards/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const MoveiCards = ({ item }) => {
   return (
-    <div className="p-1 md:p-2 items-start space-y-1 flex flex-col justify-center w-fit">
+    <div className="p-2 md:p-2 items-start space-y-1 flex flex-col justify-center max-h-96 sm:w-[250px] w-[115px] max-w-[170px]">
       <img
         src={`${import.meta.env.VITE_BASE_URL}/images/${
           item?.["poster-image"]
         }`}
         alt={item.name}
-        className="rounded w-auto"
+        className="rounded object-cover"
         onError={(item) => {
           item.target.src = `${
             import.meta.env.VITE_BASE_URL
